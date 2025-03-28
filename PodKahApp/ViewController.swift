5e747134add5165f8f60d90cf3feb89a0f741381
KahPiloupas <karinapiloupas@gmail.com>
2025-03-28 10:21:41 -0300

    Initial Commit

---
+++ PodKahApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodKahApp
+//
+//  Created by Karina Piloupas on 28/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PodKahAppTests/PodKahAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PodKahAppTests.swift
+//  PodKahAppTests
+//
+//  Created by Karina Piloupas on 28/03/25.
+//
+
+import Testing
+@testable import PodKahApp
+
+struct PodKahAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
